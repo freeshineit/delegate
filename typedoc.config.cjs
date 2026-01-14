30347e17f6c257feebdd27458a81c62d793d98ba
ShineShao <xiaoshaoqq@gmail.com>
2026-01-14 23:53:25 +0800

    feat: update typedoc

--- typedoc.config.cjs
+++ typedoc.config.cjs
@@ -1,11 +1,31 @@
 module.exports = {
     entryPoints: ["src/index.ts"],
     out: "docs",
-    plugin: ["typedoc-plugin-rename-defaults"],
+    plugin: [
+        "typedoc-plugin-rename-defaults",
+        "typedoc-plugin-mdn-links",
+        "typedoc-plugin-replace-text",
+    ],
     exclude: ["node_modules"],
     includeVersion: true,
     hideGenerator: true,
     disableSources: false,
     tsconfig: "tsconfig.json",
     readme: "README.md",
+    replaceText: {
+        inCodeCommentText: true,
+        inCodeCommentTags: true,
+        inMarkdown: false,
+        replacements: [
+            // {
+            //   pattern:"",
+            //   replace: '',
+            // },
+        ],
+    },
+    navigationLinks: {
+        Examples:
+            "https://github.com/freeshineit/delegate/tree/master/examples",
+        Github: "https://github.com/freeshineit/delegate",
+    },
 };
